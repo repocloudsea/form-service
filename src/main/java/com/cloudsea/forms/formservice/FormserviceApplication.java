3766bb55bb28d8855d3ae2fc15b08a8053552e7a
Shahbaz <jocnud@gmail.com>
2018-04-04 21:16:43 +0530

    Enabled CROS gobally

--- src/main/java/com/cloudsea/forms/formservice/FormserviceApplication.java
+++ src/main/java/com/cloudsea/forms/formservice/FormserviceApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@CrossOrigin
 public class FormserviceApplication {
 
     public static void main(String[] args) {
